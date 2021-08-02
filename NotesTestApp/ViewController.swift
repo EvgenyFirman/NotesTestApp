dc4bda7f4783576f72f97565c8589d68d7455292
Eugene <Eugeniolio@yandex.com>
2021-08-02 18:39:55 +0300

    base project

---
+++ NotesTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesTestApp
+//
+//  Created by Евгений Фирман on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
